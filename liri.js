b9615d0d891151f309cf7af2adbdc1543a71f323
josephwele <josiwele@gmail.com>
2019-05-09 11:20:05 -0700

    Mr.Nobody is set as default movie

--- liri.js
+++ liri.js
@@ -8,17 +8,28 @@ var input = process.argv[2];
 // inorder to get the last input from the user
 var search = process.argv.slice(3, process.argv.length).join("+");
 //fetching data by using axios inculind the user search on the url
-if (input === "movie-this") {
-    axios.get(`http://www.omdbapi.com/?t=${search}&y=&plot=short&apikey=e757fda1`)
+if (input === "movie-this" && search !== "") {
+    var url = `http://www.omdbapi.com/?t=${search}&y=&plot=short&apikey=e757fda1`;
+    fetch(url);
+} else {
+    url = `http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=e757fda1`
+    fetch(url);
+}
+
+
+
+
+function fetch(url) {
+    axios.get(url)
         .then(function(response) {
-            console.log();
             console.log(`*The movie rating is:${response.data.Title}
-                    * Year the movie came out ${response.data.Year}
-                    * IMDB Rating of the movie ${response.data.imdbRating}
-                    * Rotten Tomatoes Rating of the movie ${response.data.Ratings[1].Value}
-                    * Country where the movie was produced ${response.data.Country}
-                    * Language of the movie ${response.data.Language}
-                    * Plot of the movie ${response.data.Plot}
-                    * Actors in the movie ${response.data.Actors}`);
+ * Year the movie came out ${response.data.Year}
+ * IMDB Rating of the movie ${response.data.imdbRating}
+ * Rotten Tomatoes Rating of the movie ${response.data.Ratings[1].Value}
+ * Country where the movie was produced ${response.data.Country}
+ * Language of the movie ${response.data.Language}
+ * Plot of the movie ${response.data.Plot}
+ * Actors in the movie ${response.data.Actors}`);
+
         })
-} else console.log("not found");
+}
